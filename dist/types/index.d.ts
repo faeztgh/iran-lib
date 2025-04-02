98997f44b0da4b49df3b71f01a4040cd12f313ad
F43Z <50200066+faeztgh@users.noreply.github.com>
2025-04-02 18:47:24 +0330

    fix: index issue fixed

--- dist/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./classes/Bank.js";
-export * from "./classes/NationalId.js";
-export * from "./classes/Province.js";
-export { default as useBank } from "./hooks/useBank.js";
-export { default as useNationalId } from "./hooks/useNationalId.js";
-export { default as useIranProvincesAndCities } from "./hooks/useIranProvincesAndCities.js";

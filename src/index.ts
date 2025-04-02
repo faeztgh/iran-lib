e7d3691b8f8848088be5b5b87696a12266e99bd8
F43Z <50200066+faeztgh@users.noreply.github.com>
2025-04-02 18:28:50 +0330

    fix: overall fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-export * as Banks from "./classes/Bank.js";
-export * as NationalId from "./classes/NationalId.js";
-export * as Provinces from "./classes/Province.js";
-
-export * as useBank from "./hooks/useBank.js";
-export * as useNationalId from "./hooks/useNationalId.js";
-export * as useIranProvincesAndCities from "./hooks/useIranProvincesAndCities.js";
-
 export * from "./classes/Bank.js";
 export * from "./classes/NationalId.js";
 export * from "./classes/Province.js";
-export * from "./hooks/useBank.js";
-export * from "./hooks/useNationalId.js";
-export * from "./hooks/useIranProvincesAndCities.js";
+
+export { default as useBank } from "./hooks/useBank.js";
+export { default as useNationalId } from "./hooks/useNationalId.js";
+export { default as useIranProvincesAndCities } from "./hooks/useIranProvincesAndCities.js";

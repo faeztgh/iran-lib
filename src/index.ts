1b6e2765fdbb699955c694a34b10dd821b194b21
F43Z <50200066+faeztgh@users.noreply.github.com>
2025-04-02 18:15:11 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ export * as Banks from "./classes/Bank.js";
 export * as NationalId from "./classes/NationalId.js";
 export * as Provinces from "./classes/Province.js";
 
-export * as bank from "./data/bank/index.js";
-export * as nationalId from "./data/national-id/index.js";
-export * as iranProvincesAndCities from "./data/province-and-cities/index.js";
-
 export * as useBank from "./hooks/useBank.js";
 export * as useNationalId from "./hooks/useNationalId.js";
 export * as useIranProvincesAndCities from "./hooks/useIranProvincesAndCities.js";
@@ -13,9 +9,6 @@ export * as useIranProvincesAndCities from "./hooks/useIranProvincesAndCities.js
 export * from "./classes/Bank.js";
 export * from "./classes/NationalId.js";
 export * from "./classes/Province.js";
-export * from "./data/bank/index.js";
-export * from "./data/national-id/index.js";
-export * from "./data/province-and-cities/index.js";
 export * from "./hooks/useBank.js";
 export * from "./hooks/useNationalId.js";
 export * from "./hooks/useIranProvincesAndCities.js";
